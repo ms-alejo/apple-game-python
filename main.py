1c968359559746d6416ad5f3ef783d3fa766ca6c
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-15 22:29:41 -0700

    added fps / ticks

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pygame
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((350, 600))
+screen = pygame.display.set_mode((350, 600))  # window frame
+clock = pygame.time.Clock()  # basically FPS
 
-running = True
+running = True  # always true variable
 
 # game loop
 while running:
@@ -14,4 +15,5 @@ while running:
             pygame.quit()
             sys.exit()
 
+    clock.tick(60)  # 60 FPS
     pygame.display.update()
