14280c2424a2dbf7c02d58b89cfbbd564ae56399
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-15 22:36:29 -0700

    added sky and player image

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ pygame.init()
 screen = pygame.display.set_mode((350, 600))  # window frame
 clock = pygame.time.Clock()  # basically FPS
 
+# player
+player_image = pygame.image.load("assets/player_static.png").convert_alpha()
+
 running = True  # always true variable
+
+
+def draw():
+    screen.fill("lightblue")
+    screen.blit(player_image, (175, 300))
+
 
 # game loop
 while running:
@@ -14,6 +23,8 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    draw()
 
     clock.tick(60)  # 60 FPS
     pygame.display.update()
